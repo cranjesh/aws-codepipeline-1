9b663bcf8a5b906ae400ce810b87269040bccc05
cranjesh <63446800+cranjesh@users.noreply.github.com>
2024-04-17 16:31:26 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
-
 exports.handler = async (event) => {
     console.log(`Current working directory: ${process.cwd()}`);
 }
